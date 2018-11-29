canonical/maas-images

commit bff4eaa9c82427babbad9914f1991190a1bbf8a1

fix calling of write_files from handle_cloudconfig
bzr-revno: 376.1.2

curtin/centos7/curtin-hooks.py
@@ -347,7 +347,8 @@ def handle_cloudconfig(cfg, target):
             "No 'write_files' found from curtin.\n")
         return
 
-    write_files(cfg['cloudconfig'], target)
+    base_dir = os.path.join(target, 'etc/cloud/cloud.cfg.d')
+    write_files(cfg['cloudconfig'], base_dir)
 
 
 def main():

curtin/centos6/curtin-hooks.py
@@ -358,7 +358,8 @@ def handle_cloudconfig(cfg, target):
             "No 'write_files' found from curtin.\n")
         return
 
-    write_files(cfg['cloudconfig'], target)
+    base_dir = os.path.join(target, 'etc/cloud/cloud.cfg.d')
+    write_files(cfg['cloudconfig'], base_dir)
 
 
 def main():

commit d5d63aea70a72b1b005b69ca4d6d03ad1de1e87e

Write cloudconfig to the proper location.
Recent versions of Curtin have full internal support for CentOS.  If storage
information is included in the Curtin config the internal hooks are used,
otherwise the in image hooksare used if available. MAAS credentials, which are
used by cloud-init to finalize deployment, are sent by MAAS in the Curtin
config in both the cloudconfig and debconf_selections sections.

LP:1791189 prevents finalize from writing 90_datasource.cfg if Curtin has
internal support for CentOS as the credentials are already included in the
cloudconfig. This works when using the internal Curtin hooks or old versions of
Curtin that do not support CentOS at all. When a new version of Curtin is used
and no storage information is sent the in-image hooks are run for backwards
compatibility. bff4eaa introduced a bug which caused cloudconfig data written
by the internal hooks to be stored in the wrong location. This fixes that
location so they are always written to the correct location and credentials are
accessible in all scenarios. finalize is only needed for MAAS 1.9 as it does
not ship cloudconfig.

To publish this fix CentOS 7 has been locked to the latest CentOS image from
upstream.

LP: #1804026

conf/centos.yaml
@@ -13,10 +13,10 @@ versions:
         curtin_files: "{curtin_path}/centos7"
         # The upstream revision to create MAAS images for. All other versions
         # are ignored if this is defined.
-        revision: 1808
+        revision: 1809
         # The lp:maas-images release. This defaults to 01. Changing this will
         # cause a new build of the revision to be imported.
-        release: 02
+        # release: 02
         packages:
          # cloud-init depends on packages from EPEL
          - epel-release

curtin/centos7/curtin-hooks.py
@@ -334,8 +334,23 @@ def handle_cloudconfig(cfg, target):
             "No 'write_files' found from curtin.\n")
         return
 
-    base_dir = os.path.join(target, 'etc/cloud/cloud.cfg.d')
-    write_files(cfg['cloudconfig'], base_dir)
+    cloudconfig = cfg['cloudconfig']
+    base_dir = os.path.join(target, 'etc', 'cloud', 'cloud.cfg.d')
+    # Modern Curtin uses handle_cloudconfig which does this. This code path
+    # is only used on old versions Curtin/MAAS which may not include
+    # handle_cloudconfig.
+    # for each item in the dict
+    #   generate a path based on item key
+    #   if path is already in the item, LOG warning, and use generated path
+    for cfgname, cfgvalue in cloudconfig.items():
+        cfgpath = "50-cloudconfig-%s.cfg" % cfgname
+        if 'path' in cfgvalue:
+            sys.stderr.write(
+                "cloudconfig ignoring 'path' key in config '%s', using path=%s"
+                % (cfgname, os.path.join(base_dir, cfgpath)))
+        cfgvalue['path'] = cfgpath
+
+    write_files(cloudconfig, base_dir)
 
 
 def main():