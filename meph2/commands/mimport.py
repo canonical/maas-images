a0d231cddb6792d1153fd23d9cdaf5f1a218e2b5
Lee Trager <lee.trager@canonical.com>
2020-02-11 19:24:53 -0500

    Add support for building images with packer-maas.

--- meph2/commands/mimport.py
+++ meph2/commands/mimport.py
@@ -5,9 +5,13 @@ from configparser import ConfigParser
 from datetime import datetime
 import argparse
 import hashlib
+import json
 import os
+import re
+import shutil
 import subprocess
 import sys
+import tarfile
 import yaml
 
 from meph2 import util
@@ -283,6 +287,205 @@ def import_qcow2(
     return sha256.hexdigest()
 
 
+def import_packer_maas(args, cfgdata):
+    for name, data in cfgdata['packer-maas'].items():
+        arch = data.get('arch', 'amd64')
+        product_id = (
+            "com.ubuntu.maas.candidate:{os}-bases:{version}:{arch}".format(
+                arch=arch, **data)
+        )
+        content_id = "com.ubuntu.maas:candidate:{os}-bases-download".format(
+            **data)
+        target_product_stream = os.path.join(
+            'streams', 'v1', content_id + '.json')
+
+        product_tree = util.empty_iid_products(content_id)
+        product_tree['products'] = util.load_products(
+            args.target, [target_product_stream])
+        product_tree['updated'] = util.timestamp()
+        product_tree['datatype'] = 'image-ids'
+        if product_tree['products'].get(product_id) is None:
+            print(f"Creating new product {product_id}")
+            product_tree['products'][product_id] = {
+                'subarches': 'generic',
+                'label': 'candidate',
+                'subarch': 'generic',
+                'arch': arch,
+                'os': data['os'],
+                'version': data['version'],
+                'release': data['release'],
+                'release_title': data['release_title'],
+                'versions': {},
+            }
+
+        packer_dir = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..', '..', 'packer-maas', name))
+        if not os.path.exists(packer_dir):
+            sys.exit(f"Error: Unable to find packer directory {name}")
+
+        # Packer refuses to run if build artifacts are still around.
+        for build_artifact in [
+                'output-qemu', f"{name}.tar.gz", f"{name}.dd.gz"]:
+            build_artifact_path = os.path.join(packer_dir, build_artifact)
+            if os.path.isdir(build_artifact_path):
+                shutil.rmtree(build_artifact_path)
+            elif os.path.exists(build_artifact_path):
+                os.remove(build_artifact_path)
+
+        packer_template = data.get('template', f"{name}.json")
+
+        if 'yum_mirror' in data:
+            # A yum mirrorlist allows yum to pick the fastest mirror from
+            # a set of servers given by a remote host. A yum baseurl is a
+            # specific mirror. If 'yum_mirror' is given replace any
+            # mirrorlists used in the kickstart file with baseurls. This
+            # is needed for the builder as it whitelists hosts.
+            orig_kickstart_path = os.path.join(
+                packer_dir, 'http', f"{name}.ks")
+            kickstart_path = os.path.join(
+                packer_dir, 'http', f"{name}-maas-images.ks")
+            if os.path.exists(kickstart_path):
+                os.remove(kickstart_path)
+            mirrorlist_re = re.compile(
+                r'^.*(?P<mirrorlist>--mirrorlist=[\'"]?\S+[\'"]?$)')
+            # For CentOS 6
+            url_re = re.compile(
+                r'^\w*url\s+--url=[\'"]?(?P<url>.+/centos)\S+')
+            with open(orig_kickstart_path, 'r') as orig_kickstart, open(kickstart_path, 'w') as kickstart:
+                for line in orig_kickstart:
+                    mirrorlist_m = mirrorlist_re.search(line)
+                    url_m = url_re.search(line)
+                    if mirrorlist_m is not None:
+                        mirrorlist = mirrorlist_m.group('mirrorlist')
+                        mirrorlist_query = mirrorlist.split('/')[-1]
+                        if '?' in mirrorlist_query:
+                            mirrorlist_query = mirrorlist_query.split('?', 1)[1]
+                        if mirrorlist_query.endswith(("'", '"')):
+                            mirrorlist_query = mirrorlist_query[:-1]
+                        repo = {'yum_mirror': data['yum_mirror']}
+                        for q in mirrorlist_query.split('&'):
+                            k, v = q.split('=', 2)
+                            repo[k] = v
+                        # url and repo lines define this differently...
+                        if line.startswith('url'):
+                            baseurl = '--url="'
+                        else:
+                            baseurl = '--baseurl="'
+                        if 'release' not in repo:
+                            repo['release'] = repo['repo']
+                        baseurl += '{yum_mirror}/{release}/{repo}/{arch}'.format(
+                            **repo)
+                        if data['version'] >= 8:
+                            baseurl += '/os'
+                        baseurl += '"'
+                        line = line.replace(mirrorlist, baseurl)
+                    elif url_m is not None:
+                        line = line.replace(url_m.group('url'), data['yum_mirror'])
+                    kickstart.write(line)
+        else:
+            kickstart_path = None
+
+        if 'curtin_hooks' in data or kickstart_path:
+            # Modify the given template.
+            template_path = os.path.join(packer_dir, packer_template)
+            with open(template_path, 'r') as f:
+                template = json.load(f)
+            # Use the kickstart file edited above.
+            if kickstart_path:
+                for builder in template['builders']:
+                    for i, cmd in enumerate(builder['boot_command']):
+                        if cmd.startswith("inst.ks"):
+                            builder['boot_command'][i] = cmd.replace(
+                                f"{name}.ks", f"{name}-maas-images.ks")
+            # Add the given Curtin hooks when creating the tar from the
+            # disk image.
+            if 'curtin_hooks' in data:
+                curtin_path = os.path.join(
+                    os.path.dirname(__file__), "..", "..", "curtin")
+                curtin_hooks = os.path.realpath(
+                    data['curtin_hooks'].format(curtin_path=curtin_path))
+                for post_processor in template['post-processors']:
+                    if post_processor['type'] != 'shell-local':
+                        continue
+                    new_inline = []
+                    for line in post_processor['inline']:
+                        new_inline.append(line)
+                        if line == "mount /dev/nbd4p1 $TMP_DIR":
+                            new_inline.append("mkdir -p $TMP_DIR/curtin")
+                            new_inline.append(
+                                f"cp -r {curtin_hooks}/* $TMP_DIR/curtin")
+                            new_inline.append("sync $TMP_DIR/curtin")
+                    post_processor['inline'] = new_inline
+            packer_template = f"{name}-mass-images.json"
+            template_path = os.path.join(packer_dir, packer_template)
+            if os.path.exists(template_path):
+                os.remove(template_path)
+            with open(template_path, 'w') as f:
+                json.dump(template, f, indent=4)
+
+        # Set packer variables which are used to define the path to an ISO
+        # if required to build the image.
+        packer_vars = ' '.join([
+            f"-var '{key}={value}'"
+            for key,value in data.get('packer_vars', {}).items()
+        ])
+
+        # Packer must be run in the same directory as the template so the post
+        # processor can convert image into something usable by MAAS.
+        packer_path = os.environ.get('PACKER_PATH', 'packer')
+        packer_cmd = [packer_path, 'build']
+        if 'packer_vars' in data:
+            for key, value in data['packer_vars'].items():
+                packer_cmd += ['-var', f"{key}={value}"]
+        packer_cmd += [packer_template]
+        proc = subprocess.run(packer_cmd, cwd=packer_dir)
+        if proc.returncode != 0:
+            raise subprocess.CalledProcessError(
+                cmd=' '.join(packer_cmd), returncode=proc.returncode)
+
+        packer_image = os.path.join(packer_dir, f"{name}.tar.gz")
+        if os.path.exists(packer_image):
+            ftype = 'root-tgz'
+        else:
+            packer_image = os.path.join(packer_dir, f"{name}.dd.gz")
+            if not os.path.exists(packer_image):
+                sys.exit("Error: Unable to find image from Packer!")
+            ftype = 'root-dd.gz'
+
+        version_template = datetime.now().strftime("%Y%m%d") + "_%02d"
+        version_num = 1
+        version = version_template % version_num
+        while version in product_tree['products'][product_id]['versions']:
+            version_num += 1
+            version = version_template % version_num
+
+        image_path = '/'.join(
+            [data['os'], str(data['release']), arch, version, ftype])
+        real_image_path = os.path.join(
+            os.path.realpath(args.target), image_path)
+        real_image_dir = os.path.dirname(real_image_path)
+        if not os.path.exists(real_image_dir):
+            os.makedirs(real_image_dir)
+        shutil.move(packer_image, real_image_path)
+
+        product_tree['products'][product_id]['versions'][version] = {
+            'items': {
+                ftype: {
+                    'ftype': ftype,
+                    'path': image_path,
+                    **util.get_file_info(real_image_path)
+                    },
+                }
+            }
+
+        md_d = os.path.join(args.target, 'streams', 'v1')
+        if not os.path.exists(md_d):
+            os.makedirs(md_d)
+
+        with open(os.path.join(args.target, target_product_stream), 'wb') as fp:
+            fp.write(util.dump_data(product_tree))
+
+            
 def main_import(args):
     cfg_path = os.path.join(
         os.path.dirname(__file__), "..", "..", "conf", args.import_cfg)
@@ -290,35 +493,36 @@ def main_import(args):
         if os.path.exists(args.import_cfg):
             cfg_path = args.import_cfg
         else:
-            print("Error: Unable to find config file %s" % args.import_cfg)
-            sys.exit(1)
+            sys.exit("Error: Unable to find config file %s" % args.import_cfg)
 
     with open(cfg_path) as fp:
-        cfgdata = yaml.load(fp)
+        cfgdata = yaml.safe_load(fp)
 
-    target_product_stream = os.path.join(
-        'streams', 'v1', cfgdata['content_id'] + '.json')
-
-    product_tree = util.empty_iid_products(cfgdata['content_id'])
-    product_tree['products'] = util.load_products(
-        args.target, [target_product_stream])
-    product_tree['updated'] = util.timestamp()
-    product_tree['datatype'] = 'image-downloads'
-
-    if cfgdata.get('image_index') is not None:
-        import_remote_config(args, product_tree, cfgdata)
-    elif cfgdata.get('bootloaders') is not None:
-        import_bootloaders(args, product_tree, cfgdata)
+    if 'packer-maas' in cfgdata:
+        import_packer_maas(args, cfgdata)
     else:
-        sys.stderr.write('Unsupported import yaml!\n')
-        sys.exit(1)
+        target_product_stream = os.path.join(
+            'streams', 'v1', cfgdata['content_id'] + '.json')
 
-    md_d = os.path.join(args.target, 'streams', 'v1')
-    if not os.path.exists(md_d):
-        os.makedirs(md_d)
+        product_tree = util.empty_iid_products(cfgdata['content_id'])
+        product_tree['products'] = util.load_products(
+            args.target, [target_product_stream])
+        product_tree['updated'] = util.timestamp()
+        product_tree['datatype'] = 'image-downloads'
 
-    with open(os.path.join(args.target, target_product_stream), 'wb') as fp:
-        fp.write(util.dump_data(product_tree))
+        if cfgdata.get('image_index') is not None:
+            import_remote_config(args, product_tree, cfgdata)
+        elif cfgdata.get('bootloaders') is not None:
+            import_bootloaders(args, product_tree, cfgdata)
+        else:
+            sys.exit('Unsupported import yaml!\n')
+
+        md_d = os.path.join(args.target, 'streams', 'v1')
+        if not os.path.exists(md_d):
+            os.makedirs(md_d)
+
+        with open(os.path.join(args.target, target_product_stream), 'wb') as fp:
+            fp.write(util.dump_data(product_tree))
 
     util.gen_index_and_sign(args.target, not args.no_sign)
 
